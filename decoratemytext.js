f8512dac81d93688dcacdfdc53c123249ea1347e
namhmp <namhmp@users.noreply.github.com>
2020-10-04 23:09:05 -0500

    Add files via upload

--- decoratemytext.js
+++ decoratemytext.js
@@ -8,10 +8,46 @@ function biggerText(){
 	},500);
 	
 };
+function pigLatin() {
+    var text = document.getElementById('txaBox').value;
+    var arr = text.split(" ");
+    arr = Array.from(arr).map(x => {
+        if(isVowel(x.charAt(0))) {
+            return x + "ay";
+        }
+        else {
+            return x.slice(1) + x.charAt(0) + "ay";
+        }
+    });
+    document.getElementById('txaBox').value = Array.from(arr).join(" ");
+}
 
+function malkovitch() {
+    var text = document.getElementById('txaBox').value;
+    var arr = text.split(" ");
+    arr = Array.from(arr).map(x => {
+        if(x.length >= 5) {
+            return "Malkovich";
+        }
+        else
+            return x;
+    });
+    document.getElementById('txaBox').value = Array.from(arr).join(" ");
+}
+function isVowel(char) {
+    let Vowels = ['A', 'E', 'I', 'O', 'U'];
+    if (char.length > 1 || char.length === 0) {
+        return false;
+    }
+    else {
+        return Vowels.indexOf(char.toUpperCase()) >= 0;
+    }
+}
 
 window.onload = function() {
 	document.getElementById('btnBigger').onclick = biggerText;
+	document.getElementById('btnPig').onclick = pigLatin;
+	document.getElementById('btnMalko').onclick = malkovitch;
 	
 	document.getElementById('chkBling').onchange = function(){
 		var objTxaBox = document.getElementById('txaBox');
